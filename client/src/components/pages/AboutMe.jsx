f422cfbae979bdea0c9ef27afe9c0d2608c25a79
lowriegb <lowriesings@gmail.com>
2025-07-31 13:54:03 +0100

    Update AboutMe.jsx

--- client/src/components/pages/AboutMe.jsx
+++ client/src/components/pages/AboutMe.jsx
@@ -12,22 +12,22 @@ export const AboutPage = () => {
         width="100%"
       />
       <p>
-        I'm Lowrie, a 27 year old female solo singer, based in Bath (South West
+        I'm Lowrie, a 28 year old female solo singer, based in Bath (South West
         England) - I perform with top-quality backing tracks at weddings and
         events.
       </p>
       <p>
-        My favourite songs to sing includes Adele, Amy Winehouse and Alicia
+        My favourite artists to sing include Adele, Amy Winehouse and Alicia
         Keys. I absolutely love to perform a wide range of music, spanning from
-        the 1980's to current chart hits. I also love learning new songs on
-        request and discovering new tunes.
+        the 1980's to current chart hits. I really enjoy learning new songs on
+        request too, just let me know.
       </p>
       <p>
-        I have a First Class degree in Music with Performance from the
+        I have a First Class BA degree in Music with Performance from the
         University of Southampton. During my time at University, I also studied
         specialised singing for one year in Norway.
       </p>
-      <p>I have my own portable PA system and microphone.</p>
+      <p>I have my own portable, wireless PA system and microphone.</p>
       <p>
         Sit back, relax, and let me bring the cosy, acoustic vibes to your
         special day or memorable event. It doesn't matter how big or small,
