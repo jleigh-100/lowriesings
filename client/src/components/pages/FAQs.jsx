acc26a26acd27b1229dc9250ad9a73d68fea6c44
lowriegb <lowriesings@gmail.com>
2026-06-09 14:49:01 +0100

    Update FAQs.jsx
    1

--- client/src/components/pages/FAQs.jsx
+++ client/src/components/pages/FAQs.jsx
@@ -101,31 +101,34 @@ const Answer = ({ children }) => {
 const QuestionsAndAnswers = () => {
   return (
     <div style={{ marginLeft: 20, marginTop: 20 }}>
-       <Question>How do I book?</Question>
+       <Question>How can I book Lowrie Sings?</Question>
       <Answer>
-        Please enquire with me using my contact form (or send me an email via lowriesings@gmail.com). Please includ your wedding or event details, 
-        such as the date; venue; timings; and any special requests. I'll then send you a no obligation quote within 24 hours. If you would like go ahead with the booking, I'll
-        just require a completed booking form and 20% deposit to secure your date (with the remaining balance will be due 30 days before). Once I receive these, I will be very excited to confirm your booking!
+        Please send me an email via lowriesings@gmail.com. Please make sure you include all of the details about your wedding or event - including
+        the date, venue, timings, and any special requests. I'll then send you a no obligation quote within 48 hours. 
+        
+        If you are happy with the quote and would like go ahead with the booking, I'll send you my booking form and 20% deposit invoice to secure your date in my diary. Once I receive both of these back, I will then be very excited to confirm your booking!
       </Answer>
-      <Question>What happens once my booking is confirmed?</Question>
+      <Question>What happens after my booking is confirmed?</Question>
       <Answer>
-        I'll be in touch to request your selected song list around one month before your big day. For any new song requests you would like me to learn, I require at least two months notice for these. 
-      We will arrange a phone or video chat (or in-person meet up if you're local to Bath!) 
-      around 1-2 months before to discuss all of the final details! Feel free to send me a message anytime if you have any questions or would like any advice, I'm here to make sure you're comfortable and happy with everything!
+      I'll be in touch around one month before your wedding or event to request your song selections and to finalise your playlist. If there are any special song requests you'd like me to learn, please let me know at least two months in advance (up to three new songs are included with all wedding bookings).
+        
+      Around 1-2 months before your big day, we'll also arrange a phone call, video chat, or, if you're local to Bath, an in-person meeting to go over all the final details and ensure everything is going to be perfect on the day!
+        
+      In the meantime, please feel free to get in touch at any point if you have any questions or would like any advice. I'm here to make the process as smooth and stress-free as possible, and to make sure you're completely happy with every aspect of your music.
       </Answer>
       <Question>How much time do you need to set up?</Question>
       <Answer>
-        I like to arrive at least one hour before my performance time starts, so I can complete my sound check and make sure
+        I like to arrive at least one hour before for weddings, so I can complete my sound check and make sure
         everything is set up. However, I can be flexible with this as my set up only takes approximately 30 minutes, so I can set up earlier or later if necessary!
       </Answer>
       <Question>Do you bring all of the equipment you need to perform?</Question>
       <Answer>
-        Yes I provide my own PA system (I use two wireless Bose S1 Pro +) with my microphone and leads etc. My set up is entirely wirelss, which means I can set up inside or outside, 
-        or I can easily move to a different location within the venue to sing. I also have Public Liability Insurance, please let me know if your venue requires me to send them a copy of this.
+        Of course! I will bring my own PA system (I use two wireless Bose S1 Pro +), plus my microphone and leads etc. My set up is entirely wirelss, which means I can set up inside or outside, 
+        or I can easily move to a different location within the venue to sing. I also hold Public Liability Insurance up to £10 million, so do let me know if your venue requires me to send them a copy of this and I can send it across.
       </Answer>
       <Question>Can I pick the songs from the set list?</Question>
       <Answer>
-        Absolutely, I'm happy for your set list to be curated however you choose. You can select each individual song from my list or I can put together a playlist for you based on your preferences.
+        Absolutely, I'm happy for your set list to be curated however you choose. You can select each individual song from my list to completely personalise your big day, or I can put together a playlist for you based on your preferences.
       </Answer>
       <Answer>
         I'm always happy to give advice and chat through any of your preferences, so that we can create the perfect set list together!
@@ -134,7 +137,7 @@ const QuestionsAndAnswers = () => {
       <Answer>
         I'll always try my best to be flexible to help ensure your event runs
         as smoothly as possible. If your timings change slightly in the run up to the
-        event, just let me know as soon as possible and I'm sure I can accommodate this for you
+        event, just let me know as soon as possible and I'm sure I can accommodate this for you.
       </Answer>
     </div>
   )
