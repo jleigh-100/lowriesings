2702bf5f328b14ae30717d1b0a703be2c29d7442
Jamie Leigh <jamie.leigh100@hotmail.co.uk>
2026-06-09 15:55:59 +0100

    fix

--- client/src/components/pages/Testimonials.jsx
+++ client/src/components/pages/Testimonials.jsx
@@ -70,7 +70,8 @@ export const Testimonials = () => {
     {
       message:
         "Lowrie sang for our vow renewal ceremony and she was fabulous! We truly had the best day and would definitely book Lowrie again.",
-      by: "Amy, The Hilton in Bristol (19 October 2025)"    
+      by: "Amy, The Hilton in Bristol (19 October 2025)"
+    },
   ];
   const { width } = useWindowDimensions();
 
